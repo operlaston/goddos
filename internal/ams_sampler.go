f0e3bc8bd671fe188f60bdc3ff0d6ba62cb77e20
Eric Li <eric.li.8736@gmail.com>
2026-03-21 15:40:33 -0500

    Create initial AMS estimator from paper

--- internal/ams_sampler.go
+++ internal/ams_sampler.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hash/maphash"
+	"io"
+	"strings"
+)
+
+type AMSHasher struct {
+	seed maphash.Seed
+}
+
+func NewAMSHasher() *AMSHasher {
+	return &AMSHasher{
+		seed: maphash.MakeSeed(),
+	}
+}
+
+func (h *AMSHasher) Hash(item []byte) int {
+	var mh maphash.Hash
+	mh.SetSeed(h.seed)
+	mh.Write(item)
+
+	hashVal := mh.Sum64()
+
+	if hashVal&1 == 1 {
+		return 1
+	}
+	return -1
+}
+
+func ams() int {
+	r := strings.NewReader("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	hasher := NewAMSHasher()
+
+	z := 0
+	b := make([]byte, 1)
+	for {
+		_, err := r.Read(b) // might want to handle n later
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(hasher.Hash(b))
+		z += hasher.Hash(b)
+	}
+	fmt.Println(z)
+
+	return z * z
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Printf("Estimated f_2 is %v\n", ams())
 }
