eb67d39ca12c0577d0fff8245698d6c1b37b0e73
Eric Li <eric.li.8736@gmail.com>
2026-03-22 23:56:58 -0500

    Extend AMS to use median of averages for accuracy

--- internal/ams_sampler.go
+++ internal/ams_sampler.go
@@ -4,22 +4,47 @@ import (
 	"fmt"
 	"hash/maphash"
 	"io"
+	"sort"
 	"strings"
 )
 
 type AMSHasher struct {
-	seed maphash.Seed
+	seeds [][]maphash.Seed
+	buckets [][]int
+	num_buckets int
+	num_items_per_bucket int
 }
 
-func NewAMSHasher() *AMSHasher {
+func NewAMSHasher(num_buckets int, num_items_per_bucket int) *AMSHasher {
+	buckets := make([][]int, num_buckets)
+	for i := range buckets {
+		buckets[i] = make([]int, num_items_per_bucket)
+		for j := range buckets[i] {
+			buckets[i][j] = 0
+		}
+
+	}
+
+	seeds := make([][]maphash.Seed, num_buckets)
+	for i := range seeds {
+		seeds[i] = make([]maphash.Seed, num_items_per_bucket)
+		for j := range buckets[i] {
+			seeds[i][j] = maphash.MakeSeed()
+		}
+
+	}
+	
 	return &AMSHasher{
-		seed: maphash.MakeSeed(),
+		seeds: seeds,
+		buckets: buckets,
+		num_buckets: num_buckets,
+		num_items_per_bucket: num_items_per_bucket,
 	}
 }
 
-func (h *AMSHasher) Hash(item []byte) int {
+func (h *AMSHasher) Hash(item []byte, bucket_index int, seed_index int) int {
 	var mh maphash.Hash
-	mh.SetSeed(h.seed)
+	mh.SetSeed(h.seeds[bucket_index][seed_index])
 	mh.Write(item)
 
 	hashVal := mh.Sum64()
@@ -30,25 +55,54 @@ func (h *AMSHasher) Hash(item []byte) int {
 	return -1
 }
 
-func ams() int {
-	r := strings.NewReader("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	hasher := NewAMSHasher()
+func (ams* AMSHasher) Ams(b []byte, bucket_index int) {
+	for i := range ams.buckets[bucket_index] {
+		z := ams.Hash(b, bucket_index, i)
+		ams.buckets[bucket_index][i] += z
+	}
+}
 
-	z := 0
+func (ams* AMSHasher) combine() float64 {
+	bucket_averages := make([]float64, ams.num_buckets)
+
+	for i := range ams.buckets {
+		sum_of_squares := 0.0
+		for j := range ams.buckets[i] {
+			z := float64(ams.buckets[i][j])
+			sum_of_squares += z * z
+		}
+		bucket_averages[i] = sum_of_squares / float64(ams.num_items_per_bucket)
+	}
+
+	sort.Float64s(bucket_averages)
+	
+	mid := ams.num_buckets / 2
+	if ams.num_buckets%2 == 0 {
+		return (bucket_averages[mid-1] + bucket_averages[mid]) / 2.0
+	}
+	return bucket_averages[mid]
+}
+
+func Calculate_f2() float64 {
+	r := strings.NewReader("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	hasher := NewAMSHasher(21, 30)
+
 	b := make([]byte, 1)
 	for {
 		_, err := r.Read(b) // might want to handle n later
 		if err == io.EOF {
 			break
 		}
-		fmt.Println(hasher.Hash(b))
-		z += hasher.Hash(b)
+		for bucket_index := range hasher.buckets {
+			// fmt.Printf("Processing bucket %v\n", bucket_index)
+			hasher.Ams(b, bucket_index)
+		}
 	}
-	fmt.Println(z)
-
-	return z * z
+	return hasher.combine()
+	// return 0.0
 }
 
 func main() {
-	fmt.Printf("Estimated f_2 is %v\n", ams())
+	fmt.Printf("Estimated f_2 is %v\n", Calculate_f2())
 }
